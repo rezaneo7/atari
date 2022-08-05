24c153b0a30ad488961fa4ba8c58c47be00da870
rezaneo7 <31289283+rezaneo7@users.noreply.github.com>
2022-08-05 23:17:22 +0430

    Update replay.py

--- replay.py
+++ replay.py
@@ -2,36 +2,28 @@ import random, datetime
 from pathlib import Path
 
 import gym
-import gym_super_mario_bros
-from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
-from nes_py.wrappers import JoypadSpace
 
 from metrics import MetricLogger
-from agent import Mario
-from wrappers import ResizeObservation, SkipFrame
+from agent import ATARI
+from wrappers import ResizeObservation, SkipFrame, make_atari, wrap_deepmind
+import numpy as np
+env = gym.make("BreakoutNoFrameskip-v4")
 
-env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+# Use the Baseline Atari environment because of Deepmind helper functions
+env = make_atari("BreakoutNoFrameskip-v4")
 
-env = JoypadSpace(
-    env,
-    [['right'],
-    ['right', 'A']]
-)
+# Warp the frames, grey scale, stake four frame and scale to smaller ratio
+env = wrap_deepmind(env, frame_stack=True, scale=True)
 
-env = SkipFrame(env, skip=4)
-env = GrayScaleObservation(env, keep_dim=False)
-env = ResizeObservation(env, shape=84)
-env = TransformObservation(env, f=lambda x: x / 255.)
-env = FrameStack(env, num_stack=4)
 
 env.reset()
 
 save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
 save_dir.mkdir(parents=True)
 
-checkpoint = Path('checkpoints/2020-10-21T18-25-27/mario.chkpt')
-mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
-mario.exploration_rate = mario.exploration_rate_min
+checkpoint = Path('checkpoints/2020-10-21T18-25-27/atari.chkpt')
+atari = ATARI(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
+atari.exploration_rate = atari.exploration_rate_min
 
 logger = MetricLogger(save_dir)
 
@@ -40,22 +32,23 @@ episodes = 100
 for e in range(episodes):
 
     state = env.reset()
-
+    state_l = np.transpose((np.array(state)), (2, 0, 1))
     while True:
 
         env.render()
 
-        action = mario.act(state)
+        action = atari.act(state_l)
 
         next_state, reward, done, info = env.step(action)
 
-        mario.cache(state, next_state, action, reward, done)
+        next_state_l = np.transpose((np.array(next_state)), (2, 0, 1))
+        atari.cache(state_l, next_state_l, action, reward, done)
 
         logger.log_step(reward, None, None)
 
-        state = next_state
+        state_l = next_state_l
 
-        if done or info['flag_get']:
+        if done:
             break
 
     logger.log_episode()
@@ -63,6 +56,6 @@ for e in range(episodes):
     if e % 20 == 0:
         logger.record(
             episode=e,
-            epsilon=mario.exploration_rate,
-            step=mario.curr_step
+            epsilon=atari.exploration_rate,
+            step=atari.curr_step
         )
